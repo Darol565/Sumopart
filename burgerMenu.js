2862519b87426f8dc43968ef1d84579439091658
Darol565 <darol56556@gmail.com>
2024-11-01 15:09:32 +0100

    responsive

---
+++ burgerMenu.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+    const nav = document.querySelector('.nav');
+    nav.classList.toggle('nav-active');
+}
+
+// Menü schließen, wenn die Fenstergröße geändert wird
+window.addEventListener('resize', () => {
+    const nav = document.querySelector('.nav');
+    if (window.innerWidth > 768) {
+        nav.classList.remove('nav-active');
+    }
+});
